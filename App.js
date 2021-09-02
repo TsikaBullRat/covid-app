96055207c129d876b3a9d3037ecc8a30ac9313c9
TsikaBullRat <tsika1996190514@gmail.com>
2021-09-02 16:55:15 +0200

    Initial Commit

--- App.js
+++ App.js
@@ -1,21 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component, createContext, useContext } from 'react';
+import { Button,  View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import { Home } from './src/Home';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+const Drawer = createDrawerNavigator()
+export default function App(){
+    return(
+      <NavigationContainer>
+        <Drawer.Navigator screenOptions={{headerShown: false}}>
+          <Drawer.Screen 
+            name="Home">
+            {props=><Home {...props}/>}
+          </Drawer.Screen>
+          <Drawer.Screen name="Page">
+            {props=><Page {...props}/>}
+          </Drawer.Screen>
+        </Drawer.Navigator>
+      </NavigationContainer>
+    )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ src/Page.js
@@ -0,0 +1,6 @@
+import React from 'react'
+import { Text } from 'react-native'
+
+export const Page = () =>{
+    return <Text>Hello Page</Text>
+}
